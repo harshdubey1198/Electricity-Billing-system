1b2ea350f8cc1fde0aeda7bb0e37d95c1ded84e0
HARSH DUBEY <amanad08052@gmail.com>
2024-11-10 10:24:51 +0530

    timeout is increased

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true,serverSelectionTimeoutMS: 30000 })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
